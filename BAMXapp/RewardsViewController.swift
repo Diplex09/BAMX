7b216a1ac4ed51b79704e547d1a9dff0cd643702
Alonso G <71342528+AlonsoMtzG@users.noreply.github.com>
2021-09-09 17:36:27 -0500

    Created RewardsView Controller

---
+++ BAMXapp/RewardsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RewardsViewController.swift
+//  BAMXapp
+//
+//  Created by Alonso Garcia on 09/09/21.
+//
+
+import UIKit
+
+
+class RewardsViewController : UIViewController{
+    
+    @IBOutlet weak var SpecialReward: UIImageView!
+}
