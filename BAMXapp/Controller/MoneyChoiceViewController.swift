a39ec85b7c7bb804e1ff7dab9f0508af7bc4abfb
Abraham935 <Abraham_935@hotmail.com>
2021-09-10 01:41:20 -0500

    Segmented view controller implementation

--- BAMXapp/Controller/MoneyChoiceViewController.swift
+++ BAMXapp/Controller/MoneyChoiceViewController.swift
@@ -8,11 +8,25 @@
 import UIKit
 
 class MoneyChoiceViewController: UIViewController {
+    
+    @IBOutlet weak var moneyPreset: UIView!
+    @IBOutlet weak var equivalentOptions: UIView!
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    @IBAction func switchViews (_ sender: UISegmentedControl){
+        if sender.selectedSegmentIndex == 0 {
+            moneyPreset.alpha = 1
+            equivalentOptions.alpha = 0
+        }else {
+            moneyPreset.alpha = 0
+            equivalentOptions.alpha = 1
+        }
+    }
 
 }
-
