9e277bb1833a094f1bf1a4e91b69f5b8230a61ed
Manuel Cota <56351434+nihonboi@users.noreply.github.com>
2021-09-10 09:30:10 -0700

    Main Menu & Tab bar nav fixes

---
+++ BAMXapp/Model/Place.swift
@@ -0,0 +1,18 @@
+  
+//
+//  Place.swift
+//  BAMXapp
+//
+//  Created by user195828 on 9/9/21.
+//
+import Foundation
+
+struct Place: Codable{
+    var latitude: Double
+    var longitude: Double
+    
+    init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}
