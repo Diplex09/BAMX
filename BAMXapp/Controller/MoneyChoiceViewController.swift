e545990021432bfdf9d5818c29de772ca4d6804e
Abraham935 <Abraham_935@hotmail.com>
2021-10-01 19:03:04 -0500

    Updated btns of equivalent table for donations

--- BAMXapp/Controller/MoneyChoiceViewController.swift
+++ BAMXapp/Controller/MoneyChoiceViewController.swift
@@ -13,10 +13,19 @@ class MoneyChoiceViewController: UIViewController {
     @IBOutlet weak var moneyPreset: UIView!
     @IBOutlet weak var equivalentOptions: UIView!
     @IBOutlet var currentValueField: UITextField!
+    
     @IBOutlet var btn100: UIButton!
     @IBOutlet var btn200: UIButton!
     @IBOutlet var btn500: UIButton!
     @IBOutlet var btn1000: UIButton!
+    
+    //Equivalent table btns
+    @IBOutlet var btn1: UIButton!
+    @IBOutlet var btn2: UIButton!
+    @IBOutlet var btn3: UIButton!
+    @IBOutlet var btn4: UIButton!
+    @IBOutlet var btn5: UIButton!
+    
     var moneyValue: Int = 0
 
     
@@ -46,11 +55,27 @@ class MoneyChoiceViewController: UIViewController {
         
         btn500.backgroundColor = .white
         btn500.setTitleColor(.black, for: .normal)
-        
+
         btn1000.backgroundColor = .white
         btn1000.setTitleColor(.black, for: .normal)
         
+    }
+    
+    func resetColorEquivalent(){
+        btn1.backgroundColor = .white
+        btn1.setTitleColor(.systemYellow, for: .normal)
         
+        btn2.backgroundColor = .white
+        btn2.setTitleColor(.systemYellow, for: .normal)
+        
+        btn3.backgroundColor = .white
+        btn3.setTitleColor(.systemYellow, for: .normal)
+        
+        btn4.backgroundColor = .white
+        btn4.setTitleColor(.systemYellow, for: .normal)
+        
+        btn5.backgroundColor = .white
+        btn5.setTitleColor(.systemYellow, for: .normal)
     }
     
     @IBAction func didTapButton(){
@@ -61,6 +86,13 @@ class MoneyChoiceViewController: UIViewController {
     @IBAction func didTapButtonColor(sender: UIButton){
         resetColor()
         sender.backgroundColor = .red
+        sender.setTitleColor(.white, for: .normal)
+        
+    }
+    
+    @IBAction func didTapButtonColorEquivalent(sender: UIButton){
+        resetColorEquivalent()
+        sender.backgroundColor = .systemYellow
         sender.setTitleColor(.white, for: .normal)
         
     }
